e0e1053585418ac8466b8204bc9544de7908b5c5
Jeff Douglas <jeff@jeffdouglas.com>
2013-09-17 16:54:57 -0400

    finished authenticate method

--- initializers/accounts.js
+++ initializers/accounts.js
@@ -1,4 +1,5 @@
 var pg = require('pg').native
+  , async = require("async");
 
 exports.accounts = function(api, next){
 
@@ -6,15 +7,92 @@ exports.accounts = function(api, next){
 
     // methods
 
+    authenticate: function(memberName, password, next) {
+      var client = new pg.Client(api.configData.pg.connString);
+      client.connect(function(err) {
+        if (err) { console.log(err); }
+        async.waterfall([
+
+            // query pg for the user data
+            function getUser(callback){
+              var sql = "select u.username, u.isactive, m.name as memberName from public.user u inner join member__c m on m.sfdc_user__c = u.sfid where m.name = '" + memberName + "'";
+              client.query(sql, function(err, rs) {
+                if (rs['rows'].length == 1) { 
+                  callback(null, rs['rows'][0]); 
+                } else {
+                  callback(new Error("Member '"+memberName+"' not found."));
+                }
+              })
+            },
+
+            // check and see if we need to activiate user
+            function activate(user, callback){
+              if (!user.isactive) {
+                api.sfdc.org.apexRest({uri:'v.9/activate/' + user.name, method: 'GET'}, api.sfdc.oauth, function(err,resp){
+                  if(!err) { 
+                    console.log(resp);
+                    if (resp.Success == 'true') {
+                      callback(null, user);
+                    } else {
+                      callback(new Error("Could not activate '"+memberName+"'."));
+                    }
+                  }else{
+                    console.log(err);
+                    callback(new Error("Could not activate '"+memberName+"'."));
+                  }
+                })
+              } else {
+                callback(null, user);
+              }
+            },    
+            
+            // authenticate with their credentials
+            function authenticate(user, callback){
+              api.sfdc.org.authenticate({ username: user.username, password: password}, function(err, sfdc_resp){
+                // successful login
+                if (!err) { 
+                  resp = {
+                    success: true,
+                    message: "Successful sfdc login.",
+                    access_token: sfdc_resp.access_token
+                  };
+                }
+                // error logging in. return the sfdc error
+                if (err) { 
+                  resp = {
+                    success: false,
+                    message: err.message
+                  };
+                }
+                callback(null, resp);
+              });                
+            }
+        ],
+
+        // final callback
+        function(err, results){
+          if (err) {
+            resp = {
+              success: false,
+              message: err.message
+            };
+            next(resp);
+          } else {
+            next(results);
+          }
+        });   
+      })
+    },    
+
     findByService: function(service, serviceName, next) {
       var client = new pg.Client(api.configData.pg.connString);
       client.connect(function(err) {
         if (err) { console.log(err); }
 
-        // if service == 'cloudspokes'
-        api.accounts.findUserByMemberName("jeffdonthemic", function(error, results){
-          console.log(results)
-          if (results.length == 0) {
+        var sql = "select u.lastlogindate, u.fullphotourl, u.third_party_username__c, u.smallphotourl, u.alias, u.username, u.third_party_account__c, u.isactive, u.sfid as id, m.name as memberName, m.account__c, m.profile_pic__c, m.email__c from public.user u inner join member__c m on m.sfdc_user__c = u.sfid where m.name = '" + memberName + "'";
+        client.query(sql, function(err, rs) {
+
+          if (rs['rows'].length == 0) {
             var user = {
               success: false,
               message: "No user could be found with specified service and username."
@@ -28,10 +106,10 @@ exports.accounts = function(api, next){
               email: results[0].email__c, 
               accountid: results[0].account__c };            
           }
-          
+
           next(user);
-        });
-        // else if service is third party
+        })
+
       })
     },
 
@@ -39,7 +117,7 @@ exports.accounts = function(api, next){
       var client = new pg.Client(api.configData.pg.connString);
       client.connect(function(err) {
         if (err) { console.log(err); }
-        var sql = "select u.lastlogindate, u.fullphotourl, u.third_party_username__c, u.smallphotourl, u.alias, u.username, u.third_party_account__c, u.isactive, u.sfid as id, m.name as memberName, m.account__c, m.profile_pic__c, m.email__c from public.user u inner join member__c m on (m.sfdc_user__c = u.sfid) where m.name = '" + memberName + "'";
+        var sql = "select u.lastlogindate, u.fullphotourl, u.third_party_username__c, u.smallphotourl, u.alias, u.username, u.third_party_account__c, u.isactive, u.sfid as id, m.name as memberName, m.account__c, m.profile_pic__c, m.email__c from public.user u inner join member__c m on m.sfdc_user__c = u.sfid where m.name = '" + memberName + "'";
         client.query(sql, function(err, rs) {
           next(null, rs['rows']);
         })
