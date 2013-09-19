745307a367ad92b1a7c48e3984c7f73876caf5df
Jeff Douglas <jeff@jeffdouglas.com>
2013-09-19 05:56:14 -0400

    renamed find route

--- routes.js
+++ routes.js
@@ -11,9 +11,9 @@ exports.routes = {
     { path: "/:apiVersion/sponsors/:sfid", action: "sponsorsFetch" },       
     { path: "/:apiVersion/sponsors", action: "sponsorsList" },      
 
-    { path: "/:apiVersion/accounts/authenticate", action: "accountsAuthenticate" },    
-    { path: "/:apiVersion/accounts/:membername", action: "accountsFind" },      
-    { path: "/:apiVersion/accounts/find_by_service", action: "accountsFindByService" },    
+    { path: "/:apiVersion/accounts/authenticate", action: "accountsAuthenticate" },   
+    { path: "/:apiVersion/accounts/find_by_service", action: "accountsFindByService" },     
+    { path: "/:apiVersion/accounts/:membername", action: "accountsFindByName" },      
 
   ]
 
