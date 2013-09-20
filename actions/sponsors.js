a475315413ad2ccf838fcae8dd7a6bc1ac41983a
Jeff Douglas <jeff@jeffdouglas.com>
2013-09-20 08:44:28 -0400

    Fixes #70

--- actions/sponsors.js
+++ actions/sponsors.js
@@ -4,7 +4,7 @@ var forcifier = require("forcifier")
 
 exports.sponsorsList = {
   name: "sponsorsList",
-  description: "Fetches all sponsors",
+  description: "Fetches all sponsors. Method: GET",
   inputs: {
     required: [],
     optional: [],
@@ -23,16 +23,16 @@ exports.sponsorsList = {
 
 exports.sponsorsFetch = {
   name: "sponsorsFetch",
-  description: "Fetches a specific sponsor",
+  description: "Fetches a specific sponsor. Method: GET",
   inputs: {
-    required: ['sfid'],
+    required: ['id'],
     optional: [],
   },
   authenticated: false,
-  outputExample: {},
+  outputExample: { id: "001K000000f8R8aIAE", name: "Appirio", can_admin_challenges: true, funds_available: 0, logo: "http://cs-public.s3.amazonaws.com/sponsor-logos/appirio-logo.png" },
   version: 2.0,
   run: function(api, connection, next){
-    api.sponsors.fetch(connection.params.sfid, function(data){
+    api.sponsors.fetch(connection.params.id, function(data){
       utils.processResponse(data, connection);
       connection.response.count = data.length;
       next(connection, true);
