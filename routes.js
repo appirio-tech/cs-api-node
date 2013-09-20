6103654ada7b1c63c965181de1bcfdb2cd70dc1f
Jeff Douglas <jeff@jeffdouglas.com>
2013-09-20 09:38:23 -0400

    cleaned up the routes

--- routes.js
+++ routes.js
@@ -8,13 +8,22 @@ exports.routes = {
     { path: "/:apiVersion/members/:membername", action: "membersFetch" },   
     { path: "/:apiVersion/members", action: "membersList" },  
 
-    { path: "/:apiVersion/sponsors/:sfid", action: "sponsorsFetch" },       
+    { path: "/:apiVersion/sponsors/:id", action: "sponsorsFetch" },       
     { path: "/:apiVersion/sponsors", action: "sponsorsList" },      
 
-    { path: "/:apiVersion/accounts/authenticate", action: "accountsAuthenticate" },   
     { path: "/:apiVersion/accounts/find_by_service", action: "accountsFindByService" },     
-    { path: "/:apiVersion/accounts/:membername", action: "accountsFindByName" },      
+    { path: "/:apiVersion/accounts/:membername", action: "accountsFindByName" }    
 
-  ]
+  ],
+
+  put: [
+  
+  ],
+
+  post: [
+
+    { path: "/:apiVersion/accounts/authenticate", action: "accountsAuthenticate" } 
+
+  ]  
 
 };
