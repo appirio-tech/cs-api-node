69da3d254ba88b8e09ca6609927347007ae50535
Chang Shin <aproxacs@gmail.com>
2013-09-22 19:09:20 +0900

    added GET /members/:membername/payments endpoint.

--- initializers/members.js
+++ initializers/members.js
@@ -40,7 +40,36 @@ exports.members = function(api, next){
           next(rs['rows']);
         })
       })
+    },
+
+    /* 
+    * Returns payments of a specific member from pg by membername
+    *
+    * membername - the name of the member to fetch
+    * fields - the list of fields to return.  If no fields are specified then the default
+    * are passed in form the action.
+    * orderBy - the order_by option.
+    *
+    * Returns JSON containing the keys specified from the fields
+    */
+    payments: function(membername, fields, orderBy, next) {
+      var client = new pg.Client(api.configData.pg.connString);
+      client.connect(function(err) {
+        if (err) { console.log(err); }
+
+        var sql = "select "+ fields +" from payment__c p"
+                + " inner join member__c m on p.member__c = m.sfid"
+                + " inner join challenge__c c on p.challenge__c = c.sfid"
+                + " where m.name = '" + membername + "' order by " + orderBy;
+
+        console.log('debug SQL = ', sql)
+
+        client.query(sql, function(err, rs) {
+          next(rs['rows']);
+        })
+      })
     }
+
 
   }
   next();
