5d13d1d8999dbce0f009938a22dcc845662dde34
Chang Shin <aproxacs@gmail.com>
2013-09-22 23:08:28 +0900

    added/changed test using nock.
    - Recording was not fancy as VCR. It needs capturing request manually by adding `nock.recorder.rec();` code and add the captured code to the test.
    - Please regard this commit as the start point in using nock. It needs improvement.

--- test/members.js
+++ test/members.js
@@ -3,6 +3,8 @@ var request = require('request'),
     setup   = require('./setup.js'),
     _       = require("underscore")
 
+var nock = require('nock');
+
 describe('GET /members/:membername/payments', function () {
     before(function (done) {
         setup.init(done);
@@ -26,25 +28,73 @@ describe('GET /members/:membername/payments', function () {
     });
 });
 
+
 describe("GET /members/:membername/referrals", function() {
     before(function (done) {
-        setup.init(done);
+        setup.init(done);    
     });
 
-    it('mess has 1 referrals', function (done) {
-        request.get(setup.testUrl + '/members/mess/referrals', function (err, response, body) {
-            body = JSON.parse(body);
-            assert.equal(body.count, 1);
+    describe("When the member has 1 referrals", function() {
+        beforeEach(function(done) {
+            nock('https://cs9.salesforce.com:443')
+                .get('/services/apexrest/v.9/referrals/mess')
+                .reply(200, "[{\"signup_date\":\"2013-07-21T05:33:30.000Z\",\"referral_money\":0.000,\"referral_id\":\"a11K0000000ih7MIAQ\",\"profile_pic\":\"http://cs-public.s3.amazonaws.com/default_cs_member_image.png\",\"membername\":\"GHUASQ34\",\"first_year_money\":0.00}]", { date: 'Sun, 22 Sep 2013 13:34:32 GMT',
+                'content-type': 'application/json;charset=UTF-8',
+                'transfer-encoding': 'chunked' });
+        
             done();
         });
+
+        it('returned count is 1', function (done) {
+            request.get(setup.testUrl + '/members/mess/referrals', function (err, response, body) {
+                // console.log(body)
+                body = JSON.parse(body);
+                assert.equal(body.count, 1);
+                done();
+            });
+        });
+
+        it("returns 1 referral object", function(done) {
+            request.get(setup.testUrl + '/members/mess/referrals', function (err, response, body) {
+                body = JSON.parse(body);
+                assert.isNotArray(body.response);
+                done();
+            });
+        });
+
+        it("returned object has fields", function(done) {
+            request.get(setup.testUrl + '/members/mess/referrals', function (err, response, body) {
+                body = JSON.parse(body);
+                assert.propertyVal(body.response, "signup_date");
+                assert.propertyVal(body.response, "referral_money");
+                assert.propertyVal(body.response, "referral_id");
+                assert.propertyVal(body.response, "profile_pic");
+                assert.propertyVal(body.response, "membername");
+                assert.propertyVal(body.response, "first_year_money");
+                done();
+            });            
+        })
+
     });
 
-    it('jeffdonthemic does not have referrals', function (done) {
-        request.get(setup.testUrl + '/members/jeffdonthemic/referrals', function (err, response, body) {
-            body = JSON.parse(body);
-            assert.equal(body.error, "not_found");
+    describe("when the member has no refferal", function() {
+        beforeEach(function(done) {
+            nock('https://cs9.salesforce.com:443')
+              .get('/services/apexrest/v.9/referrals/jeffdonthemic')
+              .reply(200, "[]", { date: 'Sun, 22 Sep 2013 13:57:14 GMT',
+              'content-type': 'application/json;charset=UTF-8',
+              'transfer-encoding': 'chunked' });            
+
             done();
         });
-    });
+
+        it('it returns error, not_found', function (done) {
+            request.get(setup.testUrl + '/members/jeffdonthemic/referrals', function (err, response, body) {
+                body = JSON.parse(body);
+                assert.equal(body.error, "not_found");
+                done();
+            });
+        });
+    })
 
 });
--- test/setup.js
+++ test/setup.js
@@ -1,9 +1,11 @@
 var configData          = require("../config").configData,
     actionHeroPrototype = require("actionHero").actionHeroPrototype;
+var nock = require('nock');
 
 // Silence logging
-configData.logAccessToken = false;
+// configData.logAccessToken = false;
 configData.logger.transports = [];
+configData.servers.web.port = 3002;
 
 module.exports = {
   testUrl: "http://localhost:" + configData.servers.web.port + "/v1",
@@ -16,7 +18,11 @@ module.exports = {
         self.server.start({configChanges: configData}, function(err, api){
             self.api = api;
             // wait for sfdc connection. TODO : It's a temporary quick patch.
-            setTimeout(callback, 2000);
+            setTimeout(function() {
+              // nock.enableNetConnect('localhost:' + configData.servers.web.port);
+              // nock.recorder.rec();
+              callback();
+            }, 2000);
         });
     } else {
       callback();
