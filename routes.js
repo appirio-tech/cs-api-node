f025ee3be75b5925b8cb989d0335a1789049b45c
Aluxian <rosian_alex@yahoo.com>
2013-09-22 21:01:25 +0300

    #39: Implement /participants/:membername/:challenge_id/deliverables (get)

--- routes.js
+++ routes.js
@@ -24,6 +24,8 @@ exports.routes = {
     { path: "/:apiVersion/messages/inbox/:membername", action: "messagesInbox" },
     { path: "/:apiVersion/messages/:id", action: "messagesFetch" },
 
+    { path: "/:apiVersion/participants/:membername/:challenge_id/deliverables", action: "deliverablesList" },
+
     { path: "/:apiVersion/accounts/authenticate", action: "accountsAuthenticate" } , // i mad this a get so it's easier to use for now
     { path: "/:apiVersion/accounts/find_by_service", action: "accountsFindByService" },     
     { path: "/:apiVersion/accounts/:membername", action: "accountsFindByName" },
