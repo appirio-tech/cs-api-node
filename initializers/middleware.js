3b78b5727517718eea7f0793e102bec0e8bb9f3d
Jeff Douglas <jeff@jeffdouglas.com>
2013-09-27 07:53:54 -0400

    added authorization middleware. not currently being used during development

--- initializers/middleware.js
+++ initializers/middleware.js
@@ -4,33 +4,43 @@ exports.middleware = function(api, next){
 
     // parses the api_key from the Authorization request header:
     // header['Authorization'] = 'Token token="THIS-IS-MY-TOKEN"'
-  var authenticationMiddleware = function(connection, actionTemplate, next){
+  var authorizationMiddleware = function(connection, actionTemplate, next){
     if(actionTemplate.authenticated === true){
 
-      /*
-      var authorized = false;
-      var authorization = connection.rawConnection.req.headers.authorization;
+      // FOR DEVELOPMENT -- AUTHORIZE EACH REQUEST
+      return next(connection, true);
 
-      // if authorization found in the header in the headers
+      var redis = api.redis.client;
+      var authorized = false;
+      var authorization = connection.rawConnection.req.headers.authorization;   
+
+      // if authorization found in the header
       if (typeof(authorization) != "undefined") {
         try {
+
+          // parse the apiKey from: 'Token token="THIS-IS-MY-TOKEN"'
           var apiKey = S(authorization.split("=")[1]).replaceAll('"', '').s;
-          authorized = true;
+
+          // does the apiKey exist in the set of api keys in redis?
+          redis.sismember("apiKeys", apiKey, function(error, found){
+            if (found == 0) { 
+              errorResponse(connection);
+              next(connection, false); 
+            }
+            if (found == 1) { next(connection, true); }
+          });      
+
         } catch (err) {
           console.log("Could not parse API Key: " + err.message);
+          errorResponse(connection);    
+          next(connection, false);
         }
-      }
-
-      if (authorized == true) {
-        next(connection, true);
+      // if authorization not found in the header
       } else {
-        connection.response.error = "unauthorized";
-        connection.response.error_description = "This action requires authorization to continue.";
-        connection.rawConnection.responseHttpCode = 401; 
+        errorResponse(connection);
         next(connection, false);
       }
-      */
-      next(connection, true);
+
     }else{
       next(connection, true);
     }
@@ -41,7 +51,13 @@ exports.middleware = function(api, next){
     next(connection, true);
   }  
 
-  api.actions.preProcessors.push(authenticationMiddleware);
+  var errorResponse = function(connection) {
+    connection.response.error = "unauthorized";
+    connection.response.error_description = "This action requires authorization to continue.";
+    connection.rawConnection.responseHttpCode = 401;     
+  };  
+
+  api.actions.preProcessors.push(authorizationMiddleware);
   api.actions.preProcessors.push(accessTokenMiddleware);
 
   next();
